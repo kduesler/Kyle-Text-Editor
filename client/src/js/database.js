10e230155c8f8cd348ca70270e58d3ce7631f2e6
kduesler <kyle.duesler.09@gmail.com>
2023-04-27 22:22:17 -0500

    added plugins/modules

--- client/src/js/database.js
+++ client/src/js/database.js
@@ -25,7 +25,7 @@ export const putDb = async (id, content) => {
 };
 
 // TODO: Add logic for a method that gets all the content from the database
-export const getDb = async () => console.error('getDb not implemented');
+// export const getDb = async () => console.error('getDb not implemented');
 
 export const getDb = async () => {
   console.log('GET all from the database');
